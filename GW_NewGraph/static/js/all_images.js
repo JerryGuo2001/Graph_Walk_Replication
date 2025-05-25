b0812a0a775ef1f08e4a63fa33b040e7d53a551f
RohinPalsule <rohin.palsule@gmail.com>
2025-05-24 23:58:50 -0700

    fixed preload

--- GW_NewGraph/static/js/all_images.js
+++ GW_NewGraph/static/js/all_images.js
@@ -1,1 +1,11 @@
-var all_images = ['../static/images/Blue_Fabric.jpg','../static/images/isi.png']
+preload_images = []
+for (i=0;i< imageList.length;i++){
+    preload_images.push('../static/images/' + imageList[i])
+}
+preload_images.push('../static/images/isi.png','../static/images/shortest_img_1.png','../static/images/shortest_img_2.png',
+    '../static/images/isigreen.png','../static/images/isiblue.png','../static/images/GW-Tutorial/canoe.jpg', '../static/images/GW-Tutorial/chainsaw.jpg',
+    '../static/images/GW-Tutorial/golfcart.jpg', '../static/images/GW-Tutorial/trophy.jpg', '../static/images/GW-Tutorial/chess.jpg', '../static/images/GW-Tutorial/marble.jpg',
+    '../static/images/GW-Tutorial/pan.jpg', '../static/images/GW-Tutorial/heater.jpg')
+
+
+var all_images = preload_images
